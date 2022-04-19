1be7ec12b450afb60cb1e4e69c084849f06a67e5
Francisco Marcello <fmarcello265@gmail.com>
2022-04-19 17:49:22 -0400

    Auth

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { prismaClient } from "../database/prismaClient";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export class CustomerController {
   async create(req: Request, res: Response) {
     const { name, email, password, phone } = req.body;
@@ -45,5 +46,33 @@ export class CustomerController {
     });
 
     return res.status(201).json(customer);
+  }
+  async auth(req: Request, res: Response) {
+    const { email, password } = req.body;
+    if (!email) {
+      res.status(401).json({ message: "O email e obrigatorio!" });
+      return;
+    }
+    if (!password) {
+      res.status(401).json({ message: "A senha e obrigatoria!" });
+      return;
+    }
+    const user = await prismaClient.customer.findFirst({
+      where: { email: email },
+    });
+    if (!user) {
+      return res.status(401).json({ message: "Usuário não cadastrado!" });
+    }
+
+    const isValidPassword = await bcryptjs.compare(password, user.passwordHash);
+
+    if (!isValidPassword) {
+      res.status(401).json({ message: "A senha esta incorreta!" });
+    }
+    const token = jwt.sign({ id: user.id }, `${process.env.SECRET}`, {
+      expiresIn: "7d",
+    });
+    const userSPasswor = delete user.passwordHash;
+    return res.json({ user, token });
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,6 @@ const router = Router();
 const customer = new CustomerController();
 
 router.post("/customer", customer.create);
+router.post("/auth", customer.auth);
 
 export default router;
